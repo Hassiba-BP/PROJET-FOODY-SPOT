731d9021a472dbce267a344871d8b2151496edca
MarvinProInfo <71871549+MarvinProInfo@users.noreply.github.com>
2021-05-17 23:59:37 +0200

    ajout modal

---
+++ pageInscription.js
@@ -0,0 +1,20 @@
+let btn = document.getElementById('Enregistrer');
+let modal = document.getElementById('modal');
+
+
+btn.addEventListener('click',function(){
+modal.classList.add('bg-actif');
+});
+/*btn.onClick= function(){
+    modal.style.display ="Block";
+}
+
+span.onClick = function(){
+    modal.style.display ="none";
+}
+
+window.onClick = function(event){
+    if(event.target == modal){
+        modal.style.display = "none";
+    }
+}*/
